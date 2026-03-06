858f8f9591f8aeeb1063c1c5114e57e2869f4252
RishabhC-137 <rishabhguptac137@gmail.com>
2026-03-06 23:10:51 +0530

    backend deployment

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const testRoutes = require("./src/routes/testRoutes");
 
+
+const PORT = process.env.PORT || 4000;
+
+
 const app = express();
 
 app.use(cors()); // enable CORS
@@ -12,6 +16,6 @@ app.use("/screenshots", express.static("src/screenshots"));
 
 app.use("/api", testRoutes);
 
-app.listen(4000, () => {
-  console.log("Server running on port 4000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
